3eb32494c198b6090ca86a5e3f16af8b628fa656
bu7ch <dutchr@live.fr>
2017-04-12 18:31:36 +0200

    add some balises

--- js/views/appHeader.jsx
+++ js/views/appHeader.jsx
@@ -2,12 +2,14 @@ import React    from 'react';
 import { Link } from 'react-router';
 
 export default React.createClass({
-
   render() {
     return (
       <header classname="app-header">
-         app header
+         <Link to="/"><h1></h1></Link>
+         <section ClassName="account-ctrl">
+           <Link to="/users/create">Join</Link>
          <Link to="/login">Log In</Link>
+         </section>
        </header>
     );
   }
